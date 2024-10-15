7b1b1dd8ecf1886205bc227b18bc7bd4ae6ebcb1
Teja Kumar <teja.intern@srifincredit.com>
2024-10-15 09:58:03 +0530

    frontend pushes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+// src/app/config.ts
+export const API_BASE_URL = 'http://192.168.0.134:3000';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
+  
 };
